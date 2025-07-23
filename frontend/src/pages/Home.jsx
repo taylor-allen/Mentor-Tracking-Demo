d4bb19c2a056161918a4a30a674e9dfdc0d8e364
taylorallen <twhitallen07@gmail.com>
2025-07-23 10:30:04 -0500

    removed additional header

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,6 @@ import { Login } from "./Login";
 export const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold mb-4">Welcome to Mentor Tracking</h1>
       <Login />
     </div>
   );
